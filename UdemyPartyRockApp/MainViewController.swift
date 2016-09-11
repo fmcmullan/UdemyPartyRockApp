b1f9bc2e6b0d3e35cfa008190624259c51fbf078
fmcmullan <frank@mcmullan.org>
2016-09-11 17:48:13 -0400

    UI Elements added

---
+++ UdemyPartyRockApp/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UdemyPartyRockApp
+//
+//  Created by Frank on 9/11/16.
+//  Copyright © 2016 McMullan Computer Services. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
