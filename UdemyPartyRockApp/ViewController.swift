4d975c2f400dd1aeecaf614bc369cb731fedc815
fmcmullan <frank@mcmullan.org>
2016-09-11 16:20:32 -0400

    Initial Commit

---
+++ UdemyPartyRockApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UdemyPartyRockApp
+//
+//  Created by Frank on 9/11/16.
+//  Copyright © 2016 McMullan Computer Services. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
